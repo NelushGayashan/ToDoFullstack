2bebbd3d725f5d61e1e3c474f9f24108dde0b36b
Nelush Gayashan <nelushgayashan@gmail.com>
2025-04-07 19:53:34 +0530

    Add frontend code to todoappfrontend folder

---
+++ todoappfrontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskFormWithList from "./components/TaskFormWithList";
+
+const App = () => {
+  return (
+    <div>
+      <TaskFormWithList />
+    </div>
+  );
+};
+
+export default App;
